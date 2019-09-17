879bd357cd80097ca2ef5d36aa6884c2f50f7866
ScalaHub <scalahub@gmail.com>
2019-09-17 21:42:36 +0530

    Fixed ErgoMix

--- src/main/scala/org/sh/kiosk/ergo/ErgoMix.scala
+++ src/main/scala/org/sh/kiosk/ergo/ErgoMix.scala
@@ -8,65 +8,35 @@ import sigmastate.serialization.ErgoTreeSerializer.DefaultSerializer
 import special.sigma.GroupElement
 
 object ErgoMix {
-  object V1 {
-    // in v1, the value gX is hardwired in script, while in v2, it is stored in registers
-    // taken from examples
-    val fullMixScriptSource =
-      """{
-        |  val c1 = SELF.R4[GroupElement].get
-        |  val c2 = SELF.R5[GroupElement].get
-        |  proveDlog(c2) ||            // either c2 is g^y
-        |  proveDHTuple(g, c1, gX, c2) // or c2 is u^y = g^xy
-        |}""".stripMargin
+  // any variable/method starting with $ will not appear in front-end.
+  // so any variable to be hidden from front-end is prefixed with $
 
-    val halfMixScriptSource =
-      """{
-        |  val c1 = OUTPUTS(0).R4[GroupElement].get
-        |  val c2 = OUTPUTS(0).R5[GroupElement].get
-        |
-        |  OUTPUTS.size == 2 &&
-        |  OUTPUTS(0).value == SELF.value &&
-        |  OUTPUTS(1).value == SELF.value &&
-        |  blake2b256(OUTPUTS(0).propositionBytes) == fullMixScriptHash &&
-        |  blake2b256(OUTPUTS(1).propositionBytes) == fullMixScriptHash &&
-        |  OUTPUTS(1).R4[GroupElement].get == c2 &&
-        |  OUTPUTS(1).R5[GroupElement].get == c1 && {
-        |    proveDHTuple(g, gX, c1, c2) ||
-        |    proveDHTuple(g, gX, c2, c1)
-        |  }
-        |}""".stripMargin
-  }
+  val $fullMixScriptSource =
+    """{
+      |  val c1 = SELF.R4[GroupElement].get
+      |  val c2 = SELF.R5[GroupElement].get
+      |  proveDlog(c2) ||            // either c2 is g^y
+      |  proveDHTuple(g, c1, gX, c2) // or c2 is u^y = g^xy
+      |}""".stripMargin
 
-  object V2 {
-    // in v2, the value gX is stored in registers and not hardwired
-    // taken from documentation
-    val fullMixScriptSource =
-      """{
-        |  val u = SELF.R4[GroupElement].get // copied from previous transaction
-        |  val c = SELF.R5[GroupElement].get
-        |  val d = SELF.R6[GroupElement].get
-        |  proveDlog(d) || proveDHTuple(g, c, u, d)
-        |}""".stripMargin
+  val $halfMixScriptSource =
+    """{
+      |  val c1 = OUTPUTS(0).R4[GroupElement].get
+      |  val c2 = OUTPUTS(0).R5[GroupElement].get
+      |
+      |  OUTPUTS.size == 2 &&
+      |  OUTPUTS(0).value == SELF.value &&
+      |  OUTPUTS(1).value == SELF.value &&
+      |  blake2b256(OUTPUTS(0).propositionBytes) == fullMixScriptHash &&
+      |  blake2b256(OUTPUTS(1).propositionBytes) == fullMixScriptHash &&
+      |  OUTPUTS(1).R4[GroupElement].get == c2 &&
+      |  OUTPUTS(1).R5[GroupElement].get == c1 && {
+      |    proveDHTuple(g, gX, c1, c2) ||
+      |    proveDHTuple(g, gX, c2, c1)
+      |  }
+      |}""".stripMargin
 
-    val halfMixScriptSource =
-      """{
-        |  val u = SELF.R4[GroupElement].get
-        |  val u0 = OUTPUTS(0).R4[GroupElement].get
-        |  val c0 = OUTPUTS(0).R5[GroupElement].get // w0
-        |  val d0 = OUTPUTS(0).R6[GroupElement].get // w1
-        |  val u1 = OUTPUTS(1).R4[GroupElement].get
-        |  val c1 = OUTPUTS(1).R5[GroupElement].get // w1
-        |  val d1 = OUTPUTS(1).R6[GroupElement].get // w0
-        |  val bob = u0 == u && u1 == u && c0 == d1 && c1 == d0 &&
-        |            (proveDHTuple(g, u, c0, d0) || proveDHTuple(g, u, d0, c0))
-        |  val alice = proveDlog(u) // so Alice can spend if no one joins for a long time
-        |  val fullMixBox = {(b:Box) => blake2b256(b.propositionBytes) == fullMixScriptHash}
-        |  val fullMixTx = OUTPUTS(0).value == SELF.value && OUTPUTS(1).value == SELF.value &&
-        |  fullMixBox(OUTPUTS(0)) && fullMixBox(OUTPUTS(1))
-        |  fullMixTx && (bob || alice)
-        |}""".stripMargin
-  }
-  val ergoScript = new ErgoScript {}
+  val $ergoScript = new ErgoScript {}
   // any variable/method starting with $ will not appear in front-end.
   // so any variable to be hidden from front-end is prefixed with $
 
@@ -76,14 +46,11 @@ object ErgoMix {
   val $gX = SigmaDsl.GroupElement(SecP256K1.exponentiate(SecP256K1.generator, $x.bigInteger).normalize())
   val $gX_encoded = $gX.getEncoded.toArray.encodeHex
 
-  def getSourceV1 = $getSource(V1.halfMixScriptSource, V1.fullMixScriptSource)
-  def getSourceV2 = $getSource(V2.halfMixScriptSource, V2.fullMixScriptSource)
+  def getSource = $getSource($halfMixScriptSource, $fullMixScriptSource)
 
-  def getScriptsV1 = $getScripts(V1.halfMixScriptSource, V1.fullMixScriptSource, Map("g" -> $g, "gX" -> $gX))
-  def getScriptsV2 = $getScripts(V2.halfMixScriptSource, V2.fullMixScriptSource, Map("g" -> $g))
+  def getScripts = $getScripts($halfMixScriptSource, $fullMixScriptSource, Map("g" -> $g, "gX" -> $gX))
 
-  def getScriptsMatchedV1 = $getScriptsMatched(V1.halfMixScriptSource, V1.fullMixScriptSource, Map("g" -> $g, "gX" -> $gX))
-  def getScriptsMarchedV2 = $getScriptsMatched(V2.halfMixScriptSource, V2.fullMixScriptSource, Map("g" -> $g))
+  def getScriptsMatched = $getScriptsMatched($halfMixScriptSource, $fullMixScriptSource, Map("g" -> $g, "gX" -> $gX))
 
   def $getSource(halfMixScriptSource:String, fullMixScriptSource:String) = {
     Array(
@@ -93,14 +60,14 @@ object ErgoMix {
   }
 
   def $getRawScripts(halfMixScriptSource:String, fullMixScriptSource:String, env:Map[String, GroupElement]) = {
-    ergoScript.env_clear
+    $ergoScript.env_clear
     env.foreach{
-      case (name, value) => ergoScript.env_setGroupElement(name, value)
+      case (name, value) => $ergoScript.env_setGroupElement(name, value)
     }
-    val fullMixScriptBytes = DefaultSerializer.serializeErgoTree(ergoScript.$compile(fullMixScriptSource))
+    val fullMixScriptBytes = DefaultSerializer.serializeErgoTree($ergoScript.$compile(fullMixScriptSource))
     val fullMixScriptHash = scorex.crypto.hash.Blake2b256(fullMixScriptBytes)
-    ergoScript.env_setCollByte("fullMixScriptHash", fullMixScriptHash)
-    val halfMixScriptBytes = DefaultSerializer.serializeErgoTree(ergoScript.$compile(halfMixScriptSource))
+    $ergoScript.env_setCollByte("fullMixScriptHash", fullMixScriptHash)
+    val halfMixScriptBytes = DefaultSerializer.serializeErgoTree($ergoScript.$compile(halfMixScriptSource))
     (fullMixScriptBytes, halfMixScriptBytes)
   }
 
@@ -130,12 +97,12 @@ object ErgoMix {
   def $getScriptsMatched(halfMixScriptSource:String, fullMixScriptSource:String, env:Map[String, GroupElement]) = {
     val (fullMixScriptBytes, halfMixScriptBytes) = $getRawScripts(halfMixScriptSource, fullMixScriptSource, env)
 
-    ergoScript.$env.collect{
+    $ergoScript.$env.collect{
       case (keyword, value:GroupElement) =>
         keyword + " = " + value.getEncoded.toArray.encodeHex
     }.toArray ++ Array(
-      ("fullMixScript = "+ergoScript.$matchScript(fullMixScriptBytes, ergoScript.$env.keys.toArray)).grouped(120).mkString("\n"),
-      ("halfMixScript = "+ergoScript.$matchScript(halfMixScriptBytes, ergoScript.$env.keys.toArray)).grouped(120).mkString("\n")
+      ("fullMixScript = "+$ergoScript.$matchScript(fullMixScriptBytes, $ergoScript.$env.keys.toArray)).grouped(120).mkString("\n"),
+      ("halfMixScript = "+$ergoScript.$matchScript(halfMixScriptBytes, $ergoScript.$env.keys.toArray)).grouped(120).mkString("\n")
     )
 
   }
