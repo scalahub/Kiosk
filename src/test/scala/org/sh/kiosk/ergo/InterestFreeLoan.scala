90b067671cc9e836df8b20e393a263cb4d5cfa03
ScalaHub <1020304_ajsd22@users.noreply.github.com>
2019-10-29 22:43:18 +0530

    Add marginCall

--- src/test/scala/org/sh/kiosk/ergo/InterestFreeLoan.scala
+++ src/test/scala/org/sh/kiosk/ergo/InterestFreeLoan.scala
@@ -27,78 +27,82 @@ object InterestFreeLoan extends App {
       Repayment will be in secondary tokens tethered to USD.
 
    */
-  
-  val $rateOracleTokenID:Array[Byte] = Blake2b256("rate").toArray
-  val $usdTokenID:Array[Byte] = Blake2b256("USD").toArray // bob's one-way USD token
 
-  val $env = new Env
+  def runTest = {
+    val rateOracleTokenID:Array[Byte] = Blake2b256("rate").toArray
+    val usdTokenID:Array[Byte] = Blake2b256("USD").toArray // bob's one-way USD token
 
-  val $ergoScript = new ErgoScript($env) {}
-  $ergoScript.$myEnv.setCollByte("rateOracleTokenID", $rateOracleTokenID)
-  $ergoScript.$myEnv.setCollByte("usdTokenID", $usdTokenID)
+    val env = new Env
+    env.setCollByte("rateOracleTokenID", rateOracleTokenID)
+    env.setCollByte("usdTokenID", usdTokenID)
 
-  // borrower
-  val $alicePrivateKey = getRandomBigInt
-  val alice = hexToGroupElement(ECC.gExp($alicePrivateKey))
+    // borrower
+    val alicePrivateKey = getRandomBigInt
+    val alice = hexToGroupElement(ECC.gExp(alicePrivateKey))
 
-  // lender
-  val $bobPrivateKey = getRandomBigInt
-  val bob = hexToGroupElement(ECC.gExp($bobPrivateKey))
+    // lender
+    val bobPrivateKey = getRandomBigInt
+    val bob = hexToGroupElement(ECC.gExp(bobPrivateKey))
 
-  val $oneMonth = 720*30 // 720 blocks per day
-  val $fiveDays = 720*5 // 720 blocks per day
+    val oneMonth = 720*30 // 720 blocks per day
+    val fiveDays = 720*5 // 720 blocks per day
 
-  val $startDate = 10000 // block height at which loan was started
-  $ergoScript.$myEnv.setGroupElement("alice", alice)
-  $ergoScript.$myEnv.setGroupElement("bob", bob)
-  $ergoScript.$myEnv.setInt("oneMonth", $oneMonth)
-  $ergoScript.$myEnv.setInt("fiveDays", $fiveDays)
-  $ergoScript.$myEnv.setInt("emi", 1000) // equal monthly installment in USD cents
+    val startDate = 10000 // block height at which loan was started
+
+    env.setGroupElement("alice", alice)
+    env.setGroupElement("bob", bob)
+    env.setInt("oneMonth", oneMonth)
+    env.setInt("fiveDays", fiveDays)
+    env.setInt("emi", 1000) // equal monthly installment in USD cents
 
 
-  val env = $ergoScript.$myEnv.getAll
-  val ergoScript =
-    """{
-      |  val dataInput = CONTEXT.dataInputs(0)
-      |  val rate = dataInput.R4[Long].get // rate (how many USD for 1 ERG)
-      |  val correctRateOracle = dataInput.tokens(0)._1 == rateOracleTokenID
-      |
-      |  val out = OUTPUTS(0) // should be same box script
-      |  val currentUSD = SELF.R4[Long].get // how many USD owed to Bob
-      |
-      |  val lastPaymentHeight = SELF.R5[Int].get // what height last payment was made
-      |  val thisPaymentHeight = out.R5[Int].get // what the current height is
-      |
-      |  val correctHeight = thisPaymentHeight <= HEIGHT &&
-      |                      thisPaymentHeight >= HEIGHT - 720 && // within a day
-      |                      thisPaymentHeight > lastPaymentHeight
-      |
-      |  val correctScript = out.propositionBytes == SELF.propositionBytes
-      |
-      |  val outUSD = out.R4[Long].get
-      |  val usdDiff = currentUSD - outUSD
-      |  val ergsDiff = SELF.value - out.value
-      |  val correctErgsDiff = usdDiff * rate == ergsDiff
-      |
-      |  val correctDiff = usdDiff == emi && correctErgsDiff
-      |
-      |  val bobBox = OUTPUTS(1) // this is the box where Alice will pay to Bob
-      |  val correctBobAmt = bobBox.tokens(0)._1 == usdTokenID && bobBox.tokens(0)._2 == emi
-      |  val correctBobScript = bobBox.propositionBytes == proveDlog(bob).propBytes
-      |  val correctBobBox = correctBobAmt && correctBobScript
-      |
-      |  val nonPayment = (HEIGHT - lastPaymentHeight) > (oneMonth + fiveDays)
-      |
-      |  val correctTx = correctDiff && correctScript && correctRateOracle && correctHeight
-      |
-      |  correctTx && ((proveDlog(alice) && correctBobBox) || (proveDlog(bob) && nonPayment))
-      |
-      |}""".stripMargin
+    val ergoScript = new ErgoScript(env) {}
 
-  val $ergoTree = $ergoScript.$compile(ergoScript)
+    val src =
+      """{
+        |  val dataInput = CONTEXT.dataInputs(0)
+        |  val rate = dataInput.R4[Long].get // rate (how many USD for 1 ERG)
+        |  val correctRateOracle = dataInput.tokens(0)._1 == rateOracleTokenID
+        |
+        |  val out = OUTPUTS(0) // should be same box script
+        |  val currentUSD = SELF.R4[Long].get // how many USD owed to Bob
+        |
+        |  val lastPaymentHeight = SELF.R5[Int].get // what height last payment was made
+        |  val thisPaymentHeight = out.R5[Int].get // what the current height is
+        |
+        |  val correctHeight = thisPaymentHeight <= HEIGHT &&
+        |                      thisPaymentHeight >= HEIGHT - 720 && // within a day
+        |                      thisPaymentHeight > lastPaymentHeight
+        |
+        |  val correctScript = out.propositionBytes == SELF.propositionBytes
+        |
+        |  val outUSD = out.R4[Long].get
+        |  val usdDiff = currentUSD - outUSD
+        |  val ergsDiff = SELF.value - out.value
+        |  val correctErgsDiff = usdDiff * rate == ergsDiff
+        |
+        |  val correctDiff = usdDiff == emi && correctErgsDiff
+        |
+        |  val bobBox = OUTPUTS(1) // this is the box where Alice will pay to Bob
+        |  val correctBobAmt = bobBox.tokens(0)._1 == usdTokenID && bobBox.tokens(0)._2 == emi
+        |  val correctBobScript = bobBox.propositionBytes == proveDlog(bob).propBytes
+        |  val correctBobBox = correctBobAmt && correctBobScript
+        |
+        |  val nonPayment = (HEIGHT - lastPaymentHeight) > (oneMonth + fiveDays)
+        |
+        |  val correctTx = correctDiff && correctScript && correctRateOracle && correctHeight
+        |
+        |  val marginCall = currentUSD * rate > SELF.value
+        |
+        |  (marginCall && bob) || (correctTx && ((proveDlog(alice) && correctBobBox) || (proveDlog(bob) && nonPayment)))
+        |
+        |}""".stripMargin
 
-  import $ergoScript.$ergoAddressEncoder
+    val ergoTree = ergoScript.$compile(src)
 
-  println(Pay2SAddress($ergoTree))
+    import ergoScript.$ergoAddressEncoder
+
+    println(Pay2SAddress(ergoTree))
+  }
 
 }
