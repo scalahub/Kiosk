eb440c467dc0e662c586dc8c0ff4fe78a418493a
ScalaHub <1020304_ajsd22@users.noreply.github.com>
2019-11-02 20:28:48 +0530

    Added auction bugfix

--- src/test/scala/org/sh/kiosk/ergo/Auction.scala
+++ src/test/scala/org/sh/kiosk/ergo/Auction.scala
@@ -20,14 +20,14 @@ object Auction extends App {
     """{
       |  val endTime = 5000 // auction end
       |  val minBidDelta = 1000000 // nano ergs
-      |  val currBid = SELF.R4[Long].get
-      |  val currBidder = SELF.R5[GroupElement].get
+      |  val currBid = SELF.value
+      |  val currBidder = SELF.R4[GroupElement].get
       |
       |  if (HEIGHT < endTime) {
       |     val newSelf = OUTPUTS(0) // new box created as a replica of current box
       |     val oldBidRefundBox = OUTPUTS(1)
-      |     val newBid = newSelf.R4[Long].get
-      |     val newBidder = newSelf.R5[GroupElement].get // just access it to ensure that it exists
+      |     val newBid = newSelf.value
+      |     val newBidder = newSelf.R4[GroupElement].get // just access it to ensure that it exists
       |
       |     newSelf.tokens(0)._1 == SELF.tokens(0)._1 &&
       |     newSelf.tokens(0)._2 == SELF.tokens(0)._2 &&
