37bd657710b73fac2e5a4815fd050d0e2d15d17a
scalahub <23208922+scalahub@users.noreply.github.com>
2021-01-26 20:15:54 +0530

    Update public node ip

--- src/main/scala/kiosk/appkit/Client.scala
+++ src/main/scala/kiosk/appkit/Client.scala
@@ -3,19 +3,20 @@ package kiosk.appkit
 import org.ergoplatform.appkit.{BlockchainContext, ErgoClient, NetworkType, RestApiErgoClient}
 
 object Client {
-  var optClient:Option[ErgoClient] = None
-  def setClient(url:String):Int = {
+  var optClient: Option[ErgoClient] = None
+  def setClient(url: String): Int = {
     val client = RestApiErgoClient.create(url, NetworkType.MAINNET, "")
     optClient = Some(client)
     client.execute(ctx => ctx.getHeight)
   }
 
-  def usingClient[T](f: BlockchainContext => T): T= {
-    optClient.fold(throw new Exception("Set client first")){client =>
-      client.execute{ctx =>
+  def usingClient[T](f: BlockchainContext => T): T = {
+    optClient.fold(throw new Exception("Set client first")) { client =>
+      client.execute { ctx =>
         f(ctx)
       }
     }
   }
-  setClient("http://88.198.13.202:9053/")
+//  setClient("http://88.198.13.202:9053/")
+  setClient("http://159.89.116.15:9053/")
 }
