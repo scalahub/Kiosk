97313f18e27df649c660487154ed4ed9a5ab97cf
scalahub <23208922+scalahub@users.noreply.github.com>
2021-02-07 11:46:00 +0530

    Remove dependency on Play-Json

--- src/test/scala/kiosk/offchain/TraitTokenFilter.scala
+++
@@ -1,8 +0,0 @@
-package kiosk.offchain
-
-import kiosk.offchain.parser.Parser
-
-trait TraitTokenFilter {
-  val tokenFilterSource = scala.io.Source.fromFile("src/test/scala/kiosk/offchain/token-filter.json").getLines.mkString
-  val tokenFilterProtocol = Parser.parse(tokenFilterSource)
-}
